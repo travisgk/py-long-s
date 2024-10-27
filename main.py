2e05c6d59b03ec700f8fe5638c601b55f363f822
Travis G_____ <162270915+travisgk@users.noreply.github.com>
2024-10-27 14:57:52 -0500

    Further tweaks for German

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from test_conversion_func import *
 
 
 def main():
-    long_s.enable_developer_mode()
-
     # prints out sample sentences for each language.
     print("\n", end="")
     sentences = [
